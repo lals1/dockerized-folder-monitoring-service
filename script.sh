62767eabc9a9267e8e9faa1f625b6b1fd4396aec
lals1 <lals1@users.noreply.github.com>
2017-11-21 22:59:36 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,6 +4,6 @@ LOG_file = /tmp/inotify.log
 
 inotifywait -m /source -e create |
     while read path action file; do
-        echo "The file '$file' appeared in directory '$path' via '$action'" > $LOG_file
+        echo "The file '$file' appeared in directory '$path' via '$action'" >> $LOG_file
         curl -i "https://server/api?method=analyse&apikey=key&format=json&target=/source/$file" > /target/$file.json
     done

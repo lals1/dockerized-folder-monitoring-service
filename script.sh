e6795f4fb9d21d6268e602e9c9953b182d261c49
lals1 <lals1@users.noreply.github.com>
2017-11-22 11:31:08 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 
-LOG_file = /tmp/inotify.log
+logfile="/tmp/solution.log"
+
+if [ ! -f ${logfile} ]
+then
+    touch ${logfile}
+fi
 
 inotifywait -m /source -e create |
     while read path action file; do
-        echo "The file '$file' appeared in directory '$path' via '$action'" >> $LOG_file
-        curl -i "https://server/api?method=analyse&apikey=key&format=json&target=/source/$file" > /target/$file.json
+        echo "The file '$file' appeared in directory '$path' via '$action'" >> $logfile
+        curl -i "https://server/focus/api?method=analyse&apikey=key&target=/source/$file" > /target/$file.json
+        
+        if [ $? -eq 0 ]; then
+            echo "API Processing done on '$file'" >> $logfile
+        else
+            echo "API Processing failed on '$file'" >> $logfile
+
+        fi
     done
+
